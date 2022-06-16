44857a135c9646cfc9da980f046b0fa4a23de4b0
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2022-06-15 23:50:09 -0300

    interface e testes ok com as categorias

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,7 @@ class BooksController < LoggedController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
+        format.html { redirect_to book_url(@book), notice: 'Livro criado com sucesso' }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +40,7 @@ class BooksController < LoggedController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
+        format.html { redirect_to book_url(@book), notice: "Livro atualizado com sucesso" }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,7 +54,7 @@ class BooksController < LoggedController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+      format.html { redirect_to books_url, notice: "Livro deletado com sucesso" }
       format.json { head :no_content }
     end
   end
@@ -67,7 +67,7 @@ class BooksController < LoggedController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :published_at, :text, :value, :person_id)
+      params.require(:book).permit(:title, :published_at, :text, :value, :person_id,category_ids:[])
     end
 
     def load_categories
