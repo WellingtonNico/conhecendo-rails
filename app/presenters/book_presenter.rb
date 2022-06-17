648f89c4bba47f4c1c7cbf6907e3170f4666184c
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2022-06-17 11:04:33 -0300

    refatoração nas representações de livros e pessoas

--- app/presenters/book_presenter.rb
+++ app/presenters/book_presenter.rb
@@ -5,8 +5,16 @@ class BookPresenter < SimpleDelegator
         helpers.image_tag(super.to_s).html_safe
     end
 
+    def text
+        "<br/>#{super.gsub(/\n/,"<br/>")}".html_safe
+    end
+
     def value
         "R$ #{super}"
+    end
+
+    def person
+        super.name
     end
 
     private
