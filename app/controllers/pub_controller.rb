802471f5140eb76a8cd76d632172b802b0a10650
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2022-06-18 11:07:38 -0300

    melhoria no template de email de pedido

--- app/controllers/pub_controller.rb
+++ app/controllers/pub_controller.rb
@@ -54,7 +54,7 @@ class PubController < ApplicationController
       return
     end
     @cart.clear
-    OrderMailer.created(@order).deliver_now
+    OrderMailer.created(@order).deliver_later
     redirect_to order_path(@order)
   end
 
